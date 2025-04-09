bedf79a5c920b85523fe689c282f6e6c60f01c79
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:06:23 -0400

    Create ZoomIntegration.js

---
+++ frontend/src/pages/ZoomIntegration.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/ZoomIntegration.css';
+
+const ZoomIntegration = () => {
+  return (
+    <div className="zoom-integration-container">
+      <h1>Zoom Integration</h1>
+      <p>This page will manage Zoom integration settings.</p>
+    </div>
+  );
+};
+
+export default ZoomIntegration;
