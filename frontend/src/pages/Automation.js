20633d5595aed369117be5e6f9df41eac26fcbcc
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:04:13 -0400

    Create Automation.js

---
+++ frontend/src/pages/Automation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Automation.css';
+
+const Automation = () => {
+  return (
+    <div className="automation-container">
+      <h1>Automation</h1>
+      <p>This page will manage automation settings.</p>
+    </div>
+  );
+};
+
+export default Automation;
