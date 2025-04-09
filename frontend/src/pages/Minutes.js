9cae767241e9fdb95587ce1389be39408b4dc19f
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:03:20 -0400

    Create Minutes.js

---
+++ frontend/src/pages/Minutes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Minutes.css';
+
+const Minutes = () => {
+  return (
+    <div className="minutes-container">
+      <h1>Meeting Minutes</h1>
+      <p>This page will display meeting minutes.</p>
+    </div>
+  );
+};
+
+export default Minutes;
