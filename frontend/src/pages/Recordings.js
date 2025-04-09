9b0f401432dd89e3cf0c7739c3f230d7e0ed8642
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:02:18 -0400

    Create Recordings.js

---
+++ frontend/src/pages/Recordings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Recordings.css';
+
+const Recordings = () => {
+  return (
+    <div className="recordings-container">
+      <h1>Recordings</h1>
+      <p>This page will display project recordings.</p>
+    </div>
+  );
+};
+
+export default Recordings;
