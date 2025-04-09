0f0e725942b117e2a5247f4b0216c1c678f21ac1
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:07:18 -0400

    Create Distribution.js

---
+++ frontend/src/pages/Distribution.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Distribution.css';
+
+const Distribution = () => {
+  return (
+    <div className="distribution-container">
+      <h1>Distribution</h1>
+      <p>This page will manage content distribution.</p>
+    </div>
+  );
+};
+
+export default Distribution;
