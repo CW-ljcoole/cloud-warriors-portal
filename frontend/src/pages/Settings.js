ed669fc34be70cdfe5d81e928caf8ecdfc2f45f5
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:05:17 -0400

    Create Settings.js

---
+++ frontend/src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Settings.css';
+
+const Settings = () => {
+  return (
+    <div className="settings-container">
+      <h1>Settings</h1>
+      <p>This page will manage application settings.</p>
+    </div>
+  );
+};
+
+export default Settings;
