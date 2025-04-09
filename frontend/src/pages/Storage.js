d9a4a813949a72f87a1a5ac72bb07182c8b0be31
CW-ljcoole <ljcoole1998@gmail.com>
2025-04-09 13:08:15 -0400

    Create Storage.js

---
+++ frontend/src/pages/Storage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Storage.css';
+
+const Storage = () => {
+  return (
+    <div className="storage-container">
+      <h1>Storage</h1>
+      <p>This page will manage storage settings.</p>
+    </div>
+  );
+};
+
+export default Storage;
